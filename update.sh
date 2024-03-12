c463eb8baacd5bdc7c6fc27f4b3efbe1fc794c34
Jumpi08 <jumpingbeanlittlefoot@gmail.com>
2024-03-12 11:43:41 -0700

    Added log function to driver code

--- update.sh
+++ update.sh
@@ -1,44 +1,63 @@
-#Automatic Updates for miniMIA
+#!/bin/bash
 
-
-install_dir="/usr/bin/minimia-updater"
+#Constants
+install_dir="."
 log="${install_dir}/log"
 date=$(date +%D)
 time=$(date +%r)
+
+#Logging Function
+function log
+{
+	if [ -z "${1}" ]; then
+		return
+	fi
+
+	time=$(date +%r)
+	printf "[${time}] ${1}\n" >> $log
+}
 
 if ! test -f $log; then
 	touch $log
 fi
 
-echo "miniMIA Update Script" > $log
+echo "miniMIA Update Script" > $log #clears log
 echo "${date} ${time}" >> $log
-printf "\n\n" >> $log
 
 if [ "$EUID" -ne 0 ]; then 
-	echo "Script was not ran as root! Exiting..." >> $log
+	log "Script was not ran as root! Exiting..."
 	exit
 fi
 
-echo "Checking for updates..." >> $log
-#check for updates
+printf "\n" >> $log
+
+log "Checking for updates..."
 update=$(git -C $install_dir fetch --dry-run 2>&1)
 
 if [ -z $update ]; then
-	echo "No update found." >> $log
+	log "No update found."
 	#update is not required
 	exit
 fi
 
-echo "Update found!" >> $log
+log "Update found!"
 
-git -C $install_dir pull >> $log
-chmod +x "${isntall_dir}/*" >> $log
+download=$(git -C $install_dir pull 2>&1)
+log $download
+log "chmod +x directory..."
+chmod +x "${install_dir}/*"
 
-printf "\n\n" >> $log
-echo "Running all update scripts..." >> $log
+printf "\n" >> $log
+log "Running all update scripts..."
 for script in *.sh; do
-	echo "	Running Script ${script}..." >> $log
-	/bin/bash "${script}"
+	if [ $script = "update.sh" ]; then
+		continue
+	fi
+
+	printf "\n" >> $log #manual formating to make the log more clear
+	log "Running Script ${script}:"
+	source "${script}"
+	log "Finished Script ${script}\n"
 done
 
-echo "Update Finished Successfully!" >> $log
+log "Update Finished Successfully!"
